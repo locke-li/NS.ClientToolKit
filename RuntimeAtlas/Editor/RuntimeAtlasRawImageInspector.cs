1fbd0603639da1fb4fe5279ce6dadd6493da7a07
liuyunfei2020 <yunfei.liu@centurygame.com>
2021-03-11 10:06:10 +0800

    add runtime atlas code

---
+++ RuntimeAtlas/Editor/RuntimeAtlasRawImageInspector.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+using CenturyGame.RuntimeAtlas.Runtime;
+
+namespace CenturyGame.RuntimeAtlas.Editor
+{
+    [CustomEditor(typeof(RuntimeAtlasRawImage))]
+    public class RuntimeAtlasRawImageInspector : RawImageEditor
+    {
+        private Texture lastTexture = null;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            RuntimeAtlasRawImage script = target as RuntimeAtlasRawImage;
+            GUILayout.Space(5);
+            EditorGUILayout.LabelField("--------------------------------------------------------------------------------------------------------------------");
+            GUILayout.Space(5);
+            script.AtlasGroup = (RuntimeAtlasGroup)EditorGUILayout.EnumPopup("Group", script.AtlasGroup);
+            EditorGUILayout.LabelField("Texture Path", script.Path);
+            GUILayout.Space(5);
+
+            if (EditorApplication.isPlaying)
+            {
+                if (GUILayout.Button("Show RuntimeAtlas"))
+                {
+                }
+            }
+            else
+            {
+                if ((lastTexture != script.texture) || (script.texture != null && string.IsNullOrEmpty(script.Path)))
+                {
+                    lastTexture = script.texture;
+                    script.Path = AssetDatabase.GetAssetPath(script.texture);
+                }
+
+                if (script.texture == null)
+                {
+                    lastTexture = null;
+                    script.Path = string.Empty;
+                }
+            }
+            EditorGUILayout.LabelField("--------------------------------------------------------------------------------------------------------------------");
+            EditorGUILayout.LabelField("--------------------------------------------------------------------------------------------------------------------");
+        }
+    }
+}
---
+++ RuntimeAtlas/Runtime/RuntimeAtlasManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using CenturyGame.Framework.Base;
+
+namespace CenturyGame.RuntimeAtlas.Runtime
+{
+    public class RuntimeAtlasManager : Singleton<RuntimeAtlasManager>
+    {
+        private Dictionary<RuntimeAtlasGroup, RuntimeAtlas> RuntimeAtlasMap = new Dictionary<RuntimeAtlasGroup, RuntimeAtlas>();
+
+        public RuntimeAtlas GetRuntimeAtlas(RuntimeAtlasGroup group, bool topFirst)
+        {
+            RuntimeAtlas atlas;
+            if (RuntimeAtlasMap.ContainsKey(group))
+                atlas = RuntimeAtlasMap[group];
+            else
+            {
+                atlas = new RuntimeAtlas(group, topFirst);
+                RuntimeAtlasMap[group] = atlas;
+            }
+            return atlas;
+        }
+    }
+}
